910edb55a4ac050d49675bab6249538b4e063e81
Jan Frederick Eick <j.f.eick@gmx.de>
2015-03-12 07:31:54 +0100

    Fixed Splashscreen

--- app/src/main/java/de/uni_weimar/m18/anatomiederstadt/SplashActivity.java
+++ app/src/main/java/de/uni_weimar/m18/anatomiederstadt/SplashActivity.java
@@ -1,39 +1,91 @@
+/*
+ * Copyright 2015. J.F.Eick
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
 package de.uni_weimar.m18.anatomiederstadt;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
 
+import com.eftimoff.androipathview.PathView;
 
-public class SplashActivity extends ActionBarActivity {
+public class SplashActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-    }
 
+        final PathView pathView = (PathView) findViewById(R.id.pathView);
+        pathView.useNaturalColors();
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_splash, menu);
-        return true;
-    }
+        int pathDelay = 1000;
+        int pathDuration = 2500;
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        final Animation a = new AlphaAnimation(0.00f, 1.00f);
+        a.setStartOffset(3500);
+        a.setDuration(1000);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        //pathView.setSvgResource(R.raw.vitruvian_man_weimar_kk3);
+        pathView.getPathAnimator().
+                delay(pathDelay).
+                duration(pathDuration).
+                interpolator(new AccelerateDecelerateInterpolator()).
+                start();
 
-        return super.onOptionsItemSelected(item);
+        final ImageView logoImage = (ImageView) findViewById(R.id.logoImage);
+        final Context context = this;
+
+        a.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                logoImage.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+
+        logoImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        logoImage.startAnimation(a);
+
     }
 }
